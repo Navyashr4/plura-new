7e7ba9791099acfeecfa554e1063a6f14942b778
Navya Shree <navyashr4@gmail.com>
2024-08-23 13:11:41 -0500

    fixed margins

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
 
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
 
-        <p className="text-center text-2xl">Master the art of marketing your sustainable food products!</p>
+        <p className="text-center text-2xl mt-80">Master the art of marketing your sustainable food products!</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-7xl font-bold text-center md:text-[300px] tracking-tighter">
             EcoBites
